3da49764dcff8698a60b893408e06254fbf7429e
aditya kumar <adityakr2019@gmail.com>
2024-06-15 17:04:00 +0530

    commit

--- project/app/urls.py
+++ project/app/urls.py
@@ -9,5 +9,6 @@ urlpatterns = [
     path("addtocard/<int:pk>", addtocard, name='addtocard'),
 ]
 
+
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
